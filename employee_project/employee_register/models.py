1a1246930d2bbe7fe6641cc514d2174007f641ef
muthu.kumar <131681551+muthusiva249@users.noreply.github.com>
2023-12-23 18:51:02 +0530

    intial commit

---
+++ employee_project/employee_register/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.CharField(max_length=15)
+    designation = models.CharField(max_length=20,default='trainee')
+    salary = models.FloatField(max_length=15,default=10)
+
+
+class Meta:
+    db_table = "tb1employee"
