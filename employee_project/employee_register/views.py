26e30e9649366b418c68928672a9bbef2f8603ca
muthu.kumar <131681551+muthusiva249@users.noreply.github.com>
2024-01-07 01:45:18 +0530

    pagination changes

--- employee_project/employee_register/views.py
+++ employee_project/employee_register/views.py
@@ -2,11 +2,14 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import EmployeeForm
-# import sys
+from .models import Employee
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from pprint import pprint
+from django.http import HttpResponse
 
+## create employee
 def create_employee(request):
     success_message = None
-    
     if request.method == "POST":
         form = EmployeeForm(request.POST)
         if form.is_valid():
@@ -14,17 +17,35 @@ def create_employee(request):
                 form.save()
                 success_message = 'Employee created successfully!'
                 messages.success(request, success_message)
-                return redirect(reverse('create-employee'))
+                # return redirect(reverse('create-employee'))
+                return redirect('retrieve-employee')
             except:
                 pass
     else:
         form = EmployeeForm()
-        # messages.success(request, success_message)
     # return render(request, 'create.html')
     return render(request, 'create.html', {'form':form})
 
-# Search Blog
-        
+## Employee list
 def retrieve_employee(request):
-    employee = employee.objects.all()
-    return render(request,'search.html',{'employee':employee} )
+    contact_filter = request.GET.get('contact', '')
+    email_filter   = request.GET.get('email', '')
+    employee = Employee.objects.all()
+    if contact_filter:
+        employee = employee.filter(contact=contact_filter)
+    if email_filter:
+        employee = employee.filter(email=email_filter)    
+    employee = employee.order_by('-id')
+
+    # Pagination logic
+    items_per_page = 20
+    paginator = Paginator(employee, items_per_page)
+    page = request.GET.get('page', 1)
+    try:
+        employees_page = paginator.page(page)
+    except PageNotAnInteger:
+        employees_page = paginator.page(1)
+    except EmptyPage:
+        employees_page = paginator.page(paginator.num_pages)
+
+    return render(request, 'search.html', {'employee': employees_page, 'contact_filter': contact_filter,'email_filter': email_filter})
