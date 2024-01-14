205a6c19a3208f2cc43e80c58be125af09aa58b5
muthu.kumar <131681551+muthusiva249@users.noreply.github.com>
2024-01-14 22:46:08 +0530

    image change done

--- employee_project/vista_doors/views.py
+++ employee_project/vista_doors/views.py
@@ -1,29 +1,3 @@
-# from django.shortcuts import render, redirect
-# from .forms import DoorForm
-# Create your views here.
-
-# def create_door(request):
-#     if request.method == "POST":
-#         form = DoorForm(request.POST)
-#         if form.is_valid():
-#             try:
-#                 form.save()
-#                 return redirect('create-door')
-#             except:
-#                 pass
-#     else:
-#         form = DoorForm()
-#     return render(request, 'create_door.html')
-#     return render(request, 'create_door.html', {'form':form})
-
-
-# vista_doors\views.py
-
-# C:\Users\Muthu\Documents\GitHub\django_begginer\employee_project\vista_doors\views.py
-
-# cd C:\Users\Muthu\Documents\GitHub\django_begginer\employee_project
-
-
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from .forms import DoorForm
@@ -44,8 +18,8 @@ def create_door(request):
             print("Form is not valid:", form.errors)  # Print form errors for debugging
     else:
         form = DoorForm()
-
     return render(request, 'create_door.html', {'form': form})
+
 
 def shop_door(request):
     doors = Door.objects.exclude(door_image='')
